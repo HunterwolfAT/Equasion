e7ee28660efebdb4f1437266d8706f5452bf6caf
Maximilian Sieß <hunterwolf0815@gmail.com>
2014-11-29 21:23:16 +0100

    Swapped makefile for shell script, included vimrc

---
+++ compile.sh
@@ -0,0 +1,3 @@
+cd src
+g++ main.cpp LTexture.cpp ActorComponents.cpp Actor.cpp -lSDL2 -lSDL2_image -o ../bin/Equasion
+echo "Compilation successful."
---
+++ debug.sh
@@ -0,0 +1,6 @@
+clear
+cd src
+g++ main.cpp LTexture.cpp ActorComponents.cpp Actor.cpp -lSDL2 -lSDL2_image -o ../bin/Equasion &&
+echo "Compilation successful." &&
+cd ../bin
+./Equasion
