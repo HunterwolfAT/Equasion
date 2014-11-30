a924d9a5be70fe3cea9dc24367c9e61297658efd
Maximilian Sieß <hunterwolf0815@gmail.com>
2014-11-30 15:20:30 +0100

    Compile-scripts: Make bin directory if it doesnt exist

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
+mkdir bin
 cd src
 g++ main.cpp LTexture.cpp ActorComponents.cpp Actor.cpp -lSDL2 -lSDL2_image -o ../bin/Equasion &&
 echo "Compilation successful."
--- debug.sh
+++ debug.sh
@@ -1,4 +1,5 @@
 clear
+mkdir bin
 cd src
 g++ main.cpp LTexture.cpp ActorComponents.cpp Actor.cpp -lSDL2 -lSDL2_image -o ../bin/Equasion &&
 echo "Compilation successful." &&
